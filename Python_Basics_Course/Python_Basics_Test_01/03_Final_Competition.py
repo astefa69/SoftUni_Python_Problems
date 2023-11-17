8e75c5b811ca1ebcb98eada077eec7bf0e37be25
astefa69 <68390426+astefa69@users.noreply.github.com>
2023-11-17 18:51:41 +0100

    Update 03_Final_Competition.py

--- Python_Basics_Course/Python_Basics_Test_01/03_Final_Competition.py
+++ Python_Basics_Course/Python_Basics_Test_01/03_Final_Competition.py
@@ -1,7 +1,7 @@
-number_dancers = int(input()) #1.	Брой танцьори – цяло число в интервала [1 … 10]
-points = float(input()) #2.	Брой точки – реално число в интервала [1.00 … 10000.00]
-season = input() #3.	Сезон –  текст със следните възможности - "summer" или "winter"
-location = input() #4.	Място – текст със следните възможности - "Bulgaria" или "Abroad"
+number_dancers = int(input()) 
+points = float(input()) 
+season = input() 
+location = input()
 
 # charity_amount = 0
 # amount_per_dancer = 0
@@ -24,18 +24,7 @@ left_money = amount_all_dancers - charity_amount
 amount_per_dancer = left_money / number_dancers
 #amount_per_dancer = amount_all_dancers * 0.35 / number_dancers
 
-#•	Ако състезанието се е провело в България паричната награда се изчислява
-# като се умножат точките по броя участниците.
-#•	Ако се е провело в чужбина –
-# се умножават броя участници по броя точки и към тях се добавя 50% от получената сума.
-#От получената сума се изваждат разходите посочени в проценти спрямо сезона, през който се е провел:
-#След завръщането си групата дарява 75% от сумата, след приспаднатите разходи, за благотворителност.
-# Останалата сума се разпределя между членовете на групата.
-
 
 #print
-#•	Сумата, която са дали за благотворителност
-print(f"Charity - {charity_amount:.2f}") #сума за благотворителност
-#•	Сумата, която е получил всеки танцьор
-print(f"Money per dancer - {amount_per_dancer:.2f}") #сума за танцьор
-#Сумите да бъдат форматирани до втория знак след десетичната запетая.
+print(f"Charity - {charity_amount:.2f}")
+print(f"Money per dancer - {amount_per_dancer:.2f}")
