ee3fae28e5b4208ec203629cb4c8472078d432de
astefa69 <68390426+astefa69@users.noreply.github.com>
2023-12-17 23:57:35 +0100

    Update 12_trade_commissions.py

--- Python_Basics_Course/03_more_complex_conditions/lab/12_trade_commissions.py
+++ Python_Basics_Course/03_more_complex_conditions/lab/12_trade_commissions.py
@@ -52,33 +52,3 @@ elif city == "Plovdiv":
         print("error")
 else:
     print("error")
-
-
-
-#
-# #Alternative solution, solved in  class
-# #2:20
-# #https://softuni.bg/trainings/resources/video/88519/video-16-september-202 3-марио-захариев-programming-basics-with-python-september-2023/4291
-#
-# city = str(input())
-# sales = float(input())
-#
-# commission = 0
-
-# error_data = False # variable that stores the boolean value "False"
-#                   # and checks if wrong data for city, and sales volume will be entered.
-#                   # When an error is detected, error_data will take the value "True"
-#
-# if city == "Sofia":
-#     pass
-# elif city == "Varna":
-#     pass
-# elif city == "Plovdiv":
-#     pass
-# else:       # in case the entered city is wrong.
-#     error_data = True #if the city is not "Sofia", "Varna", "Plovdiv", "error_data" is activated and becomes True.
-#                         # currently the "error_data" key is off until it hits the wrong date.
-# if not error_data:   # if it's not "error_data", everything has passed through correctly, and I want to do print the sales commission
-#     print(f"{commission:.2f}")
-# else:       #if "error_data" = True, print error
-#     print("error")
