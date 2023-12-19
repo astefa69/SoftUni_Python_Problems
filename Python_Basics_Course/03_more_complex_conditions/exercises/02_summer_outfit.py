7244b6b9306c46c65c8c43cd3ff3f81bcf72d904
astefa69 <68390426+astefa69@users.noreply.github.com>
2023-12-19 17:59:15 +0100

    Create 02_summer_outfit.py

---
+++ Python_Basics_Course/03_more_complex_conditions/exercises/02_summer_outfit.py
@@ -0,0 +1,39 @@
+degrees = int(input())
+part_day = str(input())
+outfit = "" # !!!
+shoes = "" # !!!
+
+if part_day == "Morning":
+    if 10 <= degrees <= 18:
+        outfit = "Sweatshirt"
+        shoes = "Sneakers"
+    elif 18 < degrees <= 24:
+        outfit = "Shirt"
+        shoes = "Moccasins"
+    elif degrees >= 25:
+        outfit = "T-Shirt"
+        shoes = "Moccasins"
+
+elif part_day == "Afternoon":
+    if 10 <= degrees <= 18:
+        outfit = "Shirt"
+        shoes = "Moccasins"
+    elif 18 < degrees <= 24:
+        outfit = "T-Shirt"
+        shoes = "Sandals"
+    elif degrees >= 25:
+        outfit = "Swim Suit"
+        shoes = "Barefoot"
+
+elif part_day == "Evening": #else
+    if 10 <= degrees <= 18:
+        outfit = "Shirt"
+        shoes = "Moccasins"
+    elif 18 < degrees <= 24:
+        outfit = "Shirt"
+        shoes = "Moccasins"
+    elif degrees >= 25:
+        outfit = "Shirt"
+        shoes = "Moccasins"
+
+print(f"It's {degrees} degrees, get your {outfit} and {shoes}.")
