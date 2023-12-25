257a55308e56235e4260789bfe84de5583bfca83
astefa69 <68390426+astefa69@users.noreply.github.com>
2023-12-25 22:38:47 +0100

    Create 08_on_time_for_exam.py

---
+++ Python_Basics_Course/03_more_complex_conditions/exercises/08_on_time_for_exam.py
@@ -0,0 +1,33 @@
+
+hour_of_exam = int(input())
+minutes_of_exam = int(input())
+hour_of_arriving = int(input())
+minutes_of_arriving = int(input())
+
+time_of_exam = hour_of_exam * 60 + minutes_of_exam
+time_of_arriving = hour_of_arriving * 60 + minutes_of_arriving
+
+if time_of_arriving > time_of_exam:
+    print ("Late")
+elif time_of_exam - 30 <= time_of_arriving <= time_of_exam:
+    print("On time")
+elif time_of_exam - 30 > time_of_arriving: # else:
+    print ("Early")
+
+difference = abs (time_of_exam - time_of_arriving)
+hours = difference // 60
+minutes = difference % 60
+
+if time_of_exam - 60 < time_of_arriving < time_of_exam:
+    print(f"{minutes} minutes before the start")
+elif time_of_arriving <= time_of_exam - 60:
+    print(f"{hours}:{minutes:02d} hours before the start")     
+    # {minutes:02d} for adding a 0 before a number 0,1,2,6,...,9:
+    # if minutes < 10: # checks numbers 0,1,2,6,...,9 to add a zero in front of them
+    # print(f"{hours}:O{minutes} hours before the start")
+    # else:
+    # print (f"{hours}:{minutes} hours before the start")
+elif time_of_exam < time_of_arriving < time_of_exam + 60:
+    print (f"{minutes} minutes after the start")
+elif time_of_arriving >= time_of_exam + 60:
+    print (f"{hours}:{minutes:02d} hours after the start")
