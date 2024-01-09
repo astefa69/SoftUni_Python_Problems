30c35353935cd9654d90e191e55a564a456a289c
astefa69 <68390426+astefa69@users.noreply.github.com>
2024-01-09 21:04:43 +0100

    Create 09_numbers_from_1_to_10.py

---
+++ Python_Basics_Course/03_more_complex_conditions/more/09_numbers_from_1_to_10.py
@@ -0,0 +1,11 @@
+#Write a program that prints the numbers from 1 to 10, one per line.
+
+## for loop
+# for num in range(1, 10 + 1):
+#     print(num)
+
+##while loop
+num = 1
+while num <= 10:
+    print(num)
+    num += 1
